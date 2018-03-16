dc9405b744a734a92ee53957d35045235cfad4ed
maxtwardowski <twardowskimax@gmail.com>
2018-03-16 15:40:27 +0100

    Discs being drawn correctly, yet without its width considered

--- towerofhanoi.c
+++ towerofhanoi.c
@@ -13,8 +13,10 @@ void setDefault_gameboard();
 void setDefault_stack();
 void cleanScreen();
 void drawPegs(int pegs_number);
+void drawDiscs();
 void moveDisc(int peg_from, int peg_to);
 void keyDetect(int key1);
+void checkWin();
 void testPrint_gameboard();
 void testPrint_stack();
 
@@ -30,9 +32,12 @@ int main(int argc, char* argv[]) {
     while(1) {
         cleanScreen();
         drawPegs(PEGS);
+        drawDiscs();
         updateScreen();
-        testPrint_gameboard();
+        checkWin();
 
+        testPrint_gameboard();
+        testPrint_stack();
         //printf("%d\n", getkey());
 
         int keyinput1 = getkey();
@@ -78,8 +83,6 @@ void drawPegs(int pegs_number) {
         x2_base = screenWidth() / (pegs_number + 1),
         y2_base = screenHeight();
 
-    filledRect(x1_base, y1_base, x2_base, y2_base, WHITE);
-
     for (int i = 0; i < pegs_number; i++) {
         x1 += x1_base;
         x2 += x2_base;
@@ -87,13 +90,40 @@ void drawPegs(int pegs_number) {
     }
 }
 
+
+//remove duplicate variables
+void drawDiscs() {
+    int disc_width = 70, disc_height = 20;
+
+    int x1_base = screenWidth() / (PEGS + 1),
+        y1_base = screenHeight(),
+        x2_base = screenWidth() / (PEGS + 1),
+        y2_base = screenHeight() - disc_height;
+
+    int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+
+    for (int i = 0; i < PEGS; i++) {
+        x1 += x1_base;
+        y1 = y1_base;
+        x2 += x2_base;
+        y2 = y2_base;
+        for (int j = 0; j <= stack_info[i]; j++) {
+            filledRect(x1 - disc_width, y1, x2 + disc_width, y2, RED);
+            y1 -= disc_height;
+            y2 -= disc_height;
+        }
+    }
+}
+
 void moveDisc(int peg_from, int peg_to) {
     if (stack_info[peg_from] != -1) {
         int disctopush = gameboard[stack_info[peg_from]][peg_from];
-        gameboard[stack_info[peg_from]][peg_from] = 0; //Indicating empty stack slot
-        stack_info[peg_from]--; //Decreasing the origin stack index
-        stack_info[peg_to]++; //Increasing the destination stack index
-        gameboard[stack_info[peg_to]][peg_to] = disctopush;
+        if (stack_info[peg_to] == -1 || gameboard[stack_info[peg_to]][peg_to] > disctopush) {
+            gameboard[stack_info[peg_from]][peg_from] = 0; //Indicating empty stack slot
+            stack_info[peg_from]--; //Decreasing the origin stack index
+            stack_info[peg_to]++; //Increasing the destination stack index
+            gameboard[stack_info[peg_to]][peg_to] = disctopush;
+        }
     }
 }
 
@@ -111,6 +141,11 @@ void keyDetect(int key1) {
     }
 
     moveDisc(key1, key2);
+}
+
+void checkWin() {
+    if (stack_info[DISCS - 1] == DISCS - 1)
+        exit(1);
 }
 
 void testPrint_gameboard() {
